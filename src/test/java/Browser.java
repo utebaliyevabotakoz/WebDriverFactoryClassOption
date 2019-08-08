b62f640e48087690f5d4676e09cf827f59a13905
utebaliyevabotakoz <utebaliyevabotakoz@gmail.com>
2019-08-08 23:33:03 +0600

    Log4j updated + Edge configured

--- src/test/java/Browser.java
+++ src/test/java/Browser.java
@@ -3,29 +3,30 @@ import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Browser {
     private static final Logger logger = LogManager.getLogger(Browser.class);
-    public WebDriver driver;
+    public WebDriver driver1;
 
     @Test
     public void setupChrome() {
         WebDriverFactory ch = new WebDriverFactory();
-        ch.createNewDriver("Chrome");
+        driver1 =   ch.createNewDriver("chrome");
         logger.info("SET ChromeDriver");
     }
 
     @Test
     public void setupEdge() {
         WebDriverFactory ch = new WebDriverFactory();
-        ch.createNewDriver("edge");
+        driver1=  ch.createNewDriver("edge");
         logger.info("SET EdgeDriver");
     }
 
     @After
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
+        if (driver1 != null) {
+            driver1.quit();
             logger.info("Test ends");
         }
     }
--- src/test/java/WebDriverFactory.java
+++ src/test/java/WebDriverFactory.java
@@ -7,26 +7,30 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class WebDriverFactory {
     private static final Logger logger = LogManager.getLogger(Browser.class);
+    WebDriver driver;
 
-    public void createNewDriver(String name) {
-        WebDriver driver;
-
-        if (name.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-            logger.info("ChromeDriver installed");
-
-            driver.get("https://otus.ru/");
-            logger.info("OPEN https://otus.ru/");
-        }
-
-        if (name.equalsIgnoreCase("edge")) {
+    public  WebDriver createNewDriver(String name) {
+        try {
+            if (name.equalsIgnoreCase("chrome")) {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                logger.info("ChromeDriver installed");
+                driver.get("https://otus.ru/");
+                logger.info("OPEN https://otus.ru/");
+            }
+            if (name.equalsIgnoreCase("edge")) {
 //        EdgeDriverManager.getInstance().setup();
-            System.setProperty("webdriver.edge.driver", "C:\\Users\\botik\\Documents\\Otus\\Lesson5\\lesson5\\src\\test\\resources\\MicrosoftWebDriver.exe"); //put actual location
-            driver = new EdgeDriver();
-            logger.info("EdgeDriver installed");
-            driver.get("https://otus.ru/");
-            logger.info("OPEN https://otus.ru/");
+                System.setProperty("webdriver.edge.driver", "C:\\Users\\botik\\Documents\\Otus\\Lesson5\\lesson5\\src\\test\\resources\\MicrosoftWebDriver.exe"); //put actual location
+                driver = new EdgeDriver();
+                logger.info("EdgeDriver installed");
+                driver.get("https://otus.ru/");
+                logger.info("OPEN https://otus.ru/");
+            }
         }
+        catch (Exception e)
+        {
+            System.out.println("Exception" + e);
+        }
+        return driver;
     }
 }
